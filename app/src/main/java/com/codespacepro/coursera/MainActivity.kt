e5bb7afbaf085415c58a9d939db78f8cfa0da40f
Khubaib Khan <18.bscs.803@gmail.com>
2023-08-18 16:02:41 -0500

    commit

--- app/src/main/java/com/codespacepro/coursera/MainActivity.kt
+++ app/src/main/java/com/codespacepro/coursera/MainActivity.kt
@@ -5,15 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.combinedClickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.codespacepro.coursera.ui.theme.CourseraTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,14 +37,23 @@ class MainActivity : ComponentActivity() {
 fun Greeting() {
     val counter = remember { mutableStateOf(0) }
     val Longcounter = remember { mutableStateOf(0) }
-    Column {
-        Text(text = "Clicks: " + counter.value.toString()  + "Long Click: ${Longcounter.value}",modifier = Modifier.combinedClickable(
-            onClick = { counter.value += 1 },
-            onLongClick = { Longcounter.value += 2 }))
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(12.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Clicks: " + counter.value.toString() + "\nLong Click: ${Longcounter.value}",
+            modifier = Modifier.combinedClickable(
+                onClick = { counter.value += 1 },
+                onLongClick = { Longcounter.value += 2 })
+        )
         Button(
             onClick = { counter.value += 1 },
-
-        ) {
+            ) {
+            Text(text = "Click Me")
 
         }
     }
